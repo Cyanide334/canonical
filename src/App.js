2ace30e5910cc70fef2cac2b63b9d8442684e066
Muhammad Inamullahil.com <marshalllaw334@gmail.com>
2023-06-24 16:18:25 +0500

    Populate whatever data I found to be useful and apply cusotm styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import './App.css';
 import Card from './Card';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState([]);
-
-  const fetchData = () => {
-    const url = 'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json';
-    return axios.get(url).then((res) => setData(res.data))
-  };
+  const url = 'https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json';
 
   useEffect(() => {
-    fetchData();
-    console.log(data);
+    axios.get(url).then((res) => setData(res.data))
   }, []);
 
   return (
-    <div className="row">
+    <div className="row" style={{marginTop: '10px'}}>
       {data.map((item) => (
-        <div className="col-4 col-medium-3">
+        <div key={item.id} className="col-4 col-medium-3">
           <Card data={item} />
         </div>
       ))}
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,40 @@
+import moment from "moment";
+
 const Card = ({data}) => {
+    console.log(data);
+
+    // extract the author data
+    const authors = data._embedded.author.map((author) => {
+        return {
+            id : author.id,
+            name: author.name,
+            link: author.link
+        }
+    });
     
+    // extract the date
+    const date = moment(data.date).format('D MMMM YYYY');
+
+
     return (
-        <div class="p-card">
-            <div class="p-card__content">
-                <img class="p-card__image" alt="" height="185" width="330" src="https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg" />
-                <h4>
-                    <a href="#">Open Source Robotics Challenges</a>
+        <div className="p-card">
+            <div className="p-card__content">
+                <p className="u-no-margin--bottom">CLOUD AND SERVER</p>
+                <hr className="u-sv1"/>
+                <img className="p-card__image" alt="" height="185" width="330" src={data.featured_media} />
+                <h4 style={{fontWeight: 400}}>
+                    <a href={data.link}>{data.title.rendered}</a>
                 </h4>
-                <p class="u-no-padding--bottom">Open Source Robotics Challenges is a series of blogs...</p>
+                <p className="u-no-padding--bottom" >
+                    <i>By {authors.map((author, index) => (
+                        <span key={author.id}>
+                            <a href={author.link}>{author.name}</a>
+                            {index !== authors.length - 1 && ', '}
+                        </span>
+                    ))} on {date}</i>
+                </p>
+                <hr />
+                <p className="p-text--small">Article</p>
             </div>
         </div>
     );
